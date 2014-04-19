e7c493eb3e2fe1373a9a62349e5d2d89db43a312
Gaëtan Rizio <stombaker@gmail.com>
2014-04-19 11:12:31 +0200

    [1.15] Module utilisateur ajouté

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            
             new Ekologia\MainBundle\EkologiaMainBundle(),
+            new Ekologia\UserBundle\EkologiaUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Ekologia/UserBundle/EkologiaUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ekologia\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EkologiaUserBundle extends Bundle
+{
+}
